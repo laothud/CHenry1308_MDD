d3b5d13ecdaf6cec04a89f9af9a0c67ccadbdad6
Chris <laothud@yahoo.com>
2013-08-18 17:21:55 -0400

    Safe Before More errors

--- CHenry_Project/DisasterRelief/view/js/main.js
+++ CHenry_Project/DisasterRelief/view/js/main.js
@@ -1,19 +1,32 @@
-angular.module('disasterRelief', ['firebase'])
+var disasterRelief = angular.module('disasterRelief', ['firebase'])
+
+.config(function ($routeProvider){
+	$routeProvider
+	.when("/",{
+		controller:"Core",
+		templateUrl:"view/templates/home.html"
+	})
+
+	.when("/personnellogin",{
+		controller:"Core",
+		templateUrl:"view/templates/personnelLogin.html"
+	})
+
+	.otherwise({redirectTo:"/"});
+})
 
 .controller('Core', ['$scope', 'angularFireCollection', function mtCtrl($scope, angularFireCollection){
-	var url = 'https://chrishenry.firebaseio.com/disasterRelief';
+	var url = 'https://chrishenry.firebaseio.com/disasterRelief/survivors';
 
 	 //Gets the messages from firebase.
-	 $scope.survivors = angularFireCollection(url, $scope, 'survivors', []);
-
+	$scope.survivors = angularFireCollection(url, $scope, 'survivors', []);
 	var fbUser;
 
 	// Creates an instance of Firebase and connects to our URL
-	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief');
+	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief/survivors');
 
 	// instance of firebase connection to login with 
 	var auth = new FirebaseSimpleLogin(myConn, function(error, user) {
-	  console.log('Hey We\'re in', user);
 	  fbUser = user;
 	});
 	
@@ -31,4 +44,23 @@ angular.module('disasterRelief', ['firebase'])
 		auth.login('facebook');
 		$scope.addSurvivorFB();
 	}
+}])
+
+.controller('Login',['$scope', 'angularFireCollection', function mtCtrl($scope, angularFireCollection){
+	var url = 'https://chrishenry.firebaseio.com/disasterRelief/users';
+
+	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief/users');
+
+	//Gets the messages from firebase.
+	$scope.users = angularFireCollection(url, $scope, 'users', []);
+
+	$scope.userlogin = function(){
+
+		var username = $scope.username;
+		var pass = $scope.password;
+
+		$scope.users.add({name: username, password: pass});
+
+	}
+
 }])
