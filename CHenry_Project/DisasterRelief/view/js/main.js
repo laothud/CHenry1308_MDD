13bba743983049695e490f6cb66d0ad7437bb5db
Chris <laothud@yahoo.com>
2013-08-22 09:24:10 -0400

    Recommit of current state of project

--- CHenry_Project/DisasterRelief/view/js/main.js
+++ CHenry_Project/DisasterRelief/view/js/main.js
@@ -12,22 +12,45 @@ var disasterRelief = angular.module('disasterRelief', ['firebase'])
 		templateUrl:"view/templates/personnelLogin.html"
 	})
 
+	.when("/landing", {
+		controller:"Core",
+		templateUrl:"view/templates/landing.html"
+	})
+
+	.when("/adminLanding", {
+		controller:"Core",
+		templateUrl:"view/templates/adminLanding.html"
+	})
+
+	.when("/hours", {
+		controller:"Core",
+		templateUrl:"view/templates/hours.html"
+	})
+
 	.otherwise({redirectTo:"/"});
 })
 
 .controller('Core', ['$scope', 'angularFireCollection', function mtCtrl($scope, angularFireCollection){
 	var url = 'https://chrishenry.firebaseio.com/disasterRelief/survivors';
 
+	//
+	//create a check login setup to ensure site security.
+	//
+
 	 //Gets the messages from firebase.
 	$scope.survivors = angularFireCollection(url, $scope, 'survivors', []);
 	var fbUser;
+	var clicked=false;
 
 	// Creates an instance of Firebase and connects to our URL
-	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief/survivors');
+	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief');
 
 	// instance of firebase connection to login with 
 	var auth = new FirebaseSimpleLogin(myConn, function(error, user) {
-	  fbUser = user;
+		fbUser = user;
+		if(fbUser && clicked){
+			$scope.addSurvivorFB();
+		}
 	});
 	
 	$scope.addSurvivorFB = function (){
@@ -40,9 +63,22 @@ var disasterRelief = angular.module('disasterRelief', ['firebase'])
 		$scope.SurvivorName = "";
 	}
 
+	$scope.addSurvivorUser = function (){
+		$scope.survivors.add({name: $scope.SurvivorName, status:"Confirmed"});
+		$scope.SurvivorName = "";
+	}
+
 	$scope.login = function(){
+		clicked = true;
 		auth.login('facebook');
-		$scope.addSurvivorFB();
+	}
+
+
+	$scope.userLogout = function (){
+		//delete session data and return to login.
+		console.log("Logout");
+		auth.logout();
+		window.location = "#/"
 	}
 }])
 
@@ -51,16 +87,58 @@ var disasterRelief = angular.module('disasterRelief', ['firebase'])
 
 	var myConn 	= new Firebase('https://chrishenry.firebaseio.com/disasterRelief/users');
 
-	//Gets the messages from firebase.
+	//Gets the users from firebase.
 	$scope.users = angularFireCollection(url, $scope, 'users', []);
 
-	$scope.userlogin = function(){
+	var auth = new FirebaseSimpleLogin(myConn, function(error, user) {
+		if (user) {
+			
+		};
+	});
 
-		var username = $scope.username;
-		var pass = $scope.password;
+	$scope.addAdmin = function (){
+		if (user.id != users[i].id) {
+			$scope.users.add({id: user.id, email: user.email})
+		};
+	}
 
-		$scope.users.add({name: username, password: pass});
+	$scope.addUser = function(){
+		//add user to database as admin and to the email databse.
+		auth.createUser($scope.email, $scope.pass, function(error, user) {
+		  if (!error) {
+		  	if($scope.admin){
+		  		console.log('User Id: ' + user.id + ', Email: ' + user.email);
+		    	$scope.users.add({id: user.id, email: user.email});
+		  	}else{
+		  		console.log('User Id: ' + user.id + ', Email: ' + user.email);
+		  	}
+		  }else{
+		  	console.log(error);
+		  }
+		});
+	}
 
+	$scope.userlogin = function (){
+
+		for (var i = $scope.users.length - 1; i >= 0; i--) {
+			if ($scope.users[i].email == $scope.email) {
+				auth.login('password', {
+				  email: $scope.email,
+				  password: $scope.password
+				});
+				window.location = "#/landing";
+				console.log("login clicked");
+			}else{
+				auth.login('password', {
+				  email: $scope.email,
+				  password: $scope.password
+				});
+				window.location = "#/landing";
+				console.log("login clicked");
+			};
+			
+		};
 	}
 
 }])
+
